6d32af58be811f16537d8e0e897e2140687896a5
Geoff Walmsley <mathsboy@gmail.com>
2012-01-15 22:33:20 +1300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharebox::Application.config.secret_token = 'c6b8e2a8876135f1a64deca3666400e540e0c229308d23dcf80cd7ebc0ed492f37316eb662f36819a35adb1d31bf3fa5b3f6111f110ae1ae35dfdfa9f13abbb6'
