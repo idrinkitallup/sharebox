32c43a6e727d9b4d0e76258e46b266f9d60b08a7
Geoff Walmsley <mathsboy@gmail.com>
2012-01-23 22:42:05 +1300

    playing with mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,17 @@
 class UserMailer < ActionMailer::Base
   default :from => "mathsboy@gmail.com"
-  
-  def invitation_to_share(shared_folder)  
-      @shared_folder = shared_folder 
-      mail( :to => @shared_folder.shared_email,   
-            :subject => "#{@shared_folder.user.name} wants to share '#{@shared_folder.folder.name}' folder with you" )  
+      
+    def invitation_to_share(shared_folder)  
+            @shared_folder = shared_folder 
+            mail( :to => @shared_folder.shared_email,   
+                  :subject => "#{@shared_folder.user.name} wants to share '#{@shared_folder.folder.name}' folder with you" )  
     end
+    
+    def welcome_email(user)
+        @user = user
+        @url  = "http://example.com/login"
+        mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+    end
+          
+          
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {  
  :address              => "smtp.gmail.com",  
- :port                 => 587,  
+ :port                 => 25,  
  :domain               => "gmail.com",  
  :user_name            => "mathsboy",  
  :password             => "GXWalms12",  
- :authentication       => "plain",  
+ :authentication       => :login,  
  :enable_starttls_auto => true  
 }
