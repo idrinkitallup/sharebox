718679387d33b03c65df36337234187251f18f39
Geoff Walmsley <mathsboy@gmail.com>
2012-01-27 00:28:37 +1300

    editing ux and standardising

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
  :address              => "smtp.gmail.com",  
  :port                 => 25,  
  :domain               => "gmail.com",  
- :user_name            => "mathsboy",  
- :password             => "GXWalms12",  
+ :user_name            => "cloudshare.nz",  
+ :password             => "cloudsharemailer",  
  :authentication       => :login,  
  :enable_starttls_auto => true  
 }
